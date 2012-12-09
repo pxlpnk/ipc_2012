4c5d1819beebeaf821c1830b53c40932d067c314
at <at@an-ti.eu>
2012-12-09 19:37:59 +0100

    adding false sharing, using ATYPE

--- src/1-openmp/p2/matrix_vector_openmp/main.c
+++ src/1-openmp/p2/matrix_vector_openmp/main.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
+#include "../../../shared/util.h"
 
-void print_vector(long *v, int m);
-void print_matrix(long **matrix, int m, int n);
 
-void matrix_vector_mult_ref(long **x, long *a, int n, int m, long *y) {
+void print_vector(ATYPE *v, int m);
+void print_matrix(ATYPE **matrix, int m, int n);
+
+void matrix_vector_mult_ref(ATYPE **x, ATYPE *a, int n, int m, ATYPE *y) {
   for(int i=0; i<n; i++) {
     y[i]=0;
     for(int j=0; j<m; j++) {
@@ -15,14 +17,31 @@ void matrix_vector_mult_ref(long **x, long *a, int n, int m, long *y) {
 }
 
 
-void print_vector(long *v, int m) {
+void matrix_vector_mult_false_sharing(ATYPE **x, ATYPE *a, int n, int m, ATYPE *y) {
+  #pragma omp parallel
+  {
+    int rank = omp_get_thread_num();
+    int p = omp_get_num_threads();
+    for(int i=rank; i<n; i+=p) {
+      y[i]=0;
+      for(int j=0; j<m; j++) {
+        y[i] += x[i][j] * a[j];
+      }
+    }
+  }
+}
+
+
+
+void print_vector(ATYPE *v, int m) {
   printf("#### VECTOR\n");
   for (int i=0; i<m; i++) {
     printf("[%d] %ld\n", i, v[i]);
   }
 }
 
-void print_matrix(long **matrix, int m, int n) {
+
+void print_matrix(ATYPE **matrix, int m, int n) {
   printf("#### MATRIX\n");
   for (int i = 0; i < n; i++) {
     printf("[%d] ", i);
@@ -33,25 +52,37 @@ void print_matrix(long **matrix, int m, int n) {
   }
 }
 
+
+ATYPE sum_vector(ATYPE *v, int n) {
+  int aggregate = 0;
+  for(int i=0; i<n; i++) {
+    aggregate += v[i];
+  }
+  return aggregate;
+}
+
+
 int main (int argc, char *argv[]) {
 
+  printf("setting up data structures\n");
   // preparing data structures
 
   // matrix
-  int n = 1000000;
-  int m = 1000000;
-  long ** matrix;
-  long *vector;
-  long *product;
+  int n = 100000;
+  int m = 10000;
+  ATYPE ** matrix;
+  ATYPE *vector;
+  ATYPE *product;
+  ATYPE *ref_output;
 
-  matrix = (long **) malloc(sizeof(long *) * n);
+  matrix = (ATYPE **) malloc(sizeof(ATYPE *) * n);
   if (matrix == NULL) {
     fprintf(stderr, "Out of memory\n");
     return 1;
   }
 
   for (int i=0; i<n; i++) {
-    matrix[i] = (long *) malloc(sizeof(long) * m);
+    matrix[i] = (ATYPE *) malloc(sizeof(ATYPE) * m);
     if (matrix[i] == NULL) {
       fprintf(stderr,"Out of memory\n");
       return 1;
@@ -59,35 +90,60 @@ int main (int argc, char *argv[]) {
   }
 
   // vector
-  vector = (long *) malloc(sizeof(long) * m);
+  vector = (ATYPE *) malloc(sizeof(ATYPE) * m);
   if (vector == NULL) {
       fprintf(stderr,"Out of memory\n");
       return 1;
   }
 
   // resulting product
-  product = (long *) malloc(sizeof(long) *n);
+  product = (ATYPE *) malloc(sizeof(ATYPE) *n);
   if(product == NULL) {
     fprintf(stderr,"Out of memory\n");
     return 1;
   }
 
+  // reference output
+  ref_output = (long *) malloc(sizeof(long) * n);
+  if (ref_output == NULL) {
+    fprintf(stderr, "Out of memory\n");
+    return 1;
+  }
+
   // add some data to our matrix
+
   for (int i=0; i<n; i++) {
     for (int j=0; j<m; j++) {
       matrix[i][j] = ((j*i) % 42) + m;
     }
   }
 
+
   for(int i=0; i<m; i++) {
     vector[i]=i+m;
   }
 
-  matrix_vector_mult_ref(matrix, vector, n, m, product);
+  printf("Starting computation:\n");
+
+
+  printf("reference:\n");
+  double ref_time = omp_get_wtime();
+  matrix_vector_mult_ref(matrix, vector, n, m, ref_output);
+	ref_time = omp_get_wtime() - ref_time;
+
+  printf("%lf seconds.\n",ref_time);
+
+
+  printf("tiling outer loop(false_sharing):\n");
+  double time = omp_get_wtime();
+  matrix_vector_mult_false_sharing(matrix, vector, n, m, product);
+  time = omp_get_wtime() - time;
+  printf("%lf seconds.\n",time);
+
+
   /* print_vector(vector,m); */
   /* print_matrix(matrix, m, n); */
   /* print_vector(product, m); */
-
 
 
   // cleaning up
@@ -97,5 +153,7 @@ int main (int argc, char *argv[]) {
   }
 
   free(matrix);
+  free(ref_output);
+  free(product);
 
 }
