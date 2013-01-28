cf8ad5b115fb7d232a41e02493ef75148ecdf9c8
Stefan Tauner <stefan.tauner@gmx.at>
2013-01-28 07:10:45 +0100

    test: add mpi

---
+++ src/scripts/mpi.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+. functions.sh
+
+#function mpi-task-speedup () {
+	#logfile="$GITROOT/data/$dir/speedup"
+#
+	#id=p
+	#p=0
+	#tries=10
+#
+	#if [ -n "$BENCHMARK" ]; then
+		#alg="custom"
+#
+		#n=`print_power2 4`
+		#run_it
+	#fi
+	#process
+#}
+
+function mpi-n () {
+	id=n
+	p=0
+	tries=5
+	#rnd_times=5
+	#rnd_exp=28
+	#rnd_seed=42
+
+	algos="custom native seq"
+
+	n="100000 `print_power2 24` `print_power2 26` `print_power2 28`"
+	test-algos-n
+}
+
+dir="3-mpi/p2"
+wd="$GITROOT/src/$dir"
+if [ -n "$BENCHMARK" ]; then
+	cd "$wd"
+	make clean
+	#make debug
+	make fast
+
+	#mpirun -v -np 576 -node 1-35  $wd/3-mpi-p2.exe -a custom -n 5368709121
+	app="mpirun -np 576 -node 1-35  $wd/3-mpi-p2.exe"
+	proc_opt="-p" # n/a
+fi
+
+#mpi-task-speedup
+mpi-n
+
+exit_func
